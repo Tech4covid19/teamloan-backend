0c371bdc5db874685f12370b023d4c45513f8886
Nuno Alves <nunalves@outlook.com>
2020-04-24 10:42:51 +0100

    remove unused inject

--- src/main/java/pt/teamloan/init/ApplicationLifecycle.java
+++ src/main/java/pt/teamloan/init/ApplicationLifecycle.java
@@ -2,7 +2,6 @@ package pt.teamloan.init;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
-import javax.inject.Inject;
 import javax.ws.rs.core.Application;
 
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
@@ -13,16 +12,12 @@ import org.jboss.logmanager.Logger;
 import io.quarkus.runtime.ShutdownEvent;
 import io.quarkus.runtime.StartupEvent;
 import pt.teamloan.authserver.AuthServerException;
-import pt.teamloan.authserver.KeycloakServiceImpl;
 
 @OpenAPIDefinition(info = @Info(title = "backend API for TeamLoan", version = "2.0.0"), security = {
 		@SecurityRequirement(name = "DEV"), @SecurityRequirement(name = "PROD") })
 @ApplicationScoped
 public class ApplicationLifecycle extends Application {
 	private static final Logger LOGGER = Logger.getLogger(ApplicationLifecycle.class.getName());
-
-	@Inject
-	KeycloakServiceImpl service;
 	
 	void onStart(@Observes StartupEvent ev) throws AuthServerException {
 		LOGGER.info("Starting teamloan-backend API...");
